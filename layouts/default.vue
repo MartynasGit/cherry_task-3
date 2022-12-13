67712cf47ddbd3e6c910f232883846733c0593ac
Martynas Gumuliauskas <Mourcius@gmail.com>
2022-12-13 14:37:26 +0200

    Created navbar, global css file.

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <div>
+                    <NuxtLink to="/">=</NuxtLink>
+                    <NuxtLink to="/">Home</NuxtLink>
+                    <NuxtLink to="/latest-news">Latest News</NuxtLink>
+                    <NuxtLink to="/reviews">Reviews</NuxtLink>
+                </div>
+                <div>
+                    <NuxtLink to="/contact">Contact Us</NuxtLink>
+                </div>
+            </nav>
+        </header>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<style scoped>
+.router-link-exact-active {
+    color: #b0695c;
+}
+</style>
