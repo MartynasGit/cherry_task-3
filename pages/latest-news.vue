785a508c558d26b009c85bee12f12463b199db91
Martynas Gumuliauskas <Mourcius@gmail.com>
2022-12-14 17:27:11 +0200

    Created fake article database.

--- pages/latest-news.vue
+++ pages/latest-news.vue
@@ -1,25 +1,23 @@
 <template>
     <div>
         <h2 class="text-5xl">Latest News</h2>
-        <div class="flex gap-1 my-10">
+        <div class="flex gap-1 my-7">
             <div v-for="p in categories">
                 <Category :category="p" />
             </div>
         </div>
         <div class="grid lg:grid-cols-3 gap-10" v-for="b in articles">
-            <!-- <div>
-                <p>Number of the Day</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum harum hic ducimus, est obcaecati
-                    vitae.</p>
-            </div> -->
-            
-
+            <p>{{b.title}}</p>
         </div>
     </div>
 </template>
 
 <script setup>
-const categories = ["Politics", "Buisness", "Tech", "Arts", "Science", "Health", "Sports"]
+const categories = ["Politics", "Finance", "Tech", "Beauty", "Science", "Food", "Sports"];
+
+const articles = await $fetch("http://localhost:3001/articles")
+
+
 </script>
 
 <style scoped>
